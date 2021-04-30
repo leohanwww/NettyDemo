6b202d701c7083d7d0379534fb21f27ff3f7e326
zhanghan <pumpkinlittlehan@hotmail.com>
2021-04-30 14:54:35 +0800

    聊天服务端和客户端

--- src/main/java/com/zhhan/netty/chart/ChatServerHandler.java
+++ src/main/java/com/zhhan/netty/chart/ChatServerHandler.java
@@ -15,21 +15,21 @@ public class ChatServerHandler extends SimpleChannelInboundHandler<String> {
 
     public static List<Channel> channels = new ArrayList<>();
 
-    @Override  //通道就绪
+    @Override  //通道就绪(用户上线)
     public void channelActive(ChannelHandlerContext ctx) {
         Channel inChannel = ctx.channel();
         channels.add(inChannel);
         System.out.println("[Server]:" + inChannel.remoteAddress().toString().substring(1) + "上线");
     }
 
-    @Override  //通道未就绪
+    @Override  //通道未就绪(用户下线)
     public void channelInactive(ChannelHandlerContext ctx) {
         Channel inChannel = ctx.channel();
         channels.remove(inChannel);
         System.out.println("[Server]:" + inChannel.remoteAddress().toString().substring(1) + "离线");
     }
 
-    @Override  //读取数据
+    @Override  //读取数据(从通道0,)
     protected void channelRead0(ChannelHandlerContext ctx, String s) {
         Channel inChannel = ctx.channel();
         for (Channel channel : channels) {
